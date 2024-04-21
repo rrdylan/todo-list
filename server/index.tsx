b92e127712d7bb30ce476ddbbd2342891a0fa913
Dylan Rose-Rosette <dylan.roserosette@gmail.com>
2024-04-21 15:22:25 +0200

    install axios

--- server/index.tsx
+++ server/index.tsx
@@ -1,4 +1,10 @@
 const express = require('express');
 const app = express();
 
-console.log("test");
+app.get('/', (request, response) => {
+    response.send('Hello world !!! ');
+});
+
+app.listen(8080,  () => {
+    console.log('listening on port 8080');
+});
