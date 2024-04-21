ebdf9c20ce4094df796f88e247693610df88b18e
Dylan Rose-Rosette <dylan.roserosette@gmail.com>
2024-04-21 16:41:10 +0200

    Test APi call

--- server/index.tsx
+++ server/index.tsx
@@ -1,5 +1,14 @@
+// Node.js framework
 const express = require('express');
+// CORS package provide Connect/Express middleware
+// CORS = Cross-Origin Resource Sharing, permet les requetes front to back
+const cors = require('cors');
+
 const app = express();
+
+
+
+app.use(cors());
 
 app.get('/', (request, response) => {
     response.send('Hello world !!! ');
--- server/src/App.tsx
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-
-const apiCall = () => {
-    axios.get('http://localhost:8080').then((data) => {
-        console.log(data);
-    });
-}
